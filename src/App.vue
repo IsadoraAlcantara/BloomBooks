8cdd4a35b15a1167e6809c77faf1306774c77ad3
giulianobre <giuliarnobre@gmail.com>
2023-11-15 18:32:44 -0300

    Acrescentando titulo e botão de pesquisa

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <script setup>
 import Magnify from 'vue-material-design-icons/Magnify.vue'
 import { ref, onMounted } from 'vue';
+ 
 
 const images = ref([
-    'https://i.pinimg.com/564x/81/b4/1b/81b41bf0249a52da787cb0b26f08214d.jpg',
-    'https://i.pinimg.com/564x/db/db/0f/dbdb0f73f16b2b7579e7189d30d4568b.jpg',
-    'https://i.pinimg.com/564x/fa/95/f4/fa95f40d078d5e444348f631a25f24f6.jpg',
-    'https://i.pinimg.com/564x/ce/a6/82/cea68267c3294713623da43dbf9aea7b.jpg',
+  'https://i.pinimg.com/564x/81/b4/1b/81b41bf0249a52da787cb0b26f08214d.jpg',
+  'https://i.pinimg.com/564x/db/db/0f/dbdb0f73f16b2b7579e7189d30d4568b.jpg',
+  'https://i.pinimg.com/564x/fa/95/f4/fa95f40d078d5e444348f631a25f24f6.jpg',
+  'https://i.pinimg.com/564x/ce/a6/82/cea68267c3294713623da43dbf9aea7b.jpg',
 
 ]);
 
 const currentIndex = ref(0);
 
 const goToSlide = (index) => {
-    currentIndex.value = index++;
+  currentIndex.value = index++;
 };
 
 const nextSlide = () => {
-    currentIndex.value = currentIndex.value === images.value.length - 1 ? 0 : currentIndex.value + 1
+  currentIndex.value = currentIndex.value === images.value.length - 1 ? 0 : currentIndex.value + 1
 }
 
 onMounted(() => {
-    setInterval(() => {
-        nextSlide()
-    }, 2000)
+  setInterval(() => {
+    nextSlide()
+  }, 2000)
 })
 </script>
 
@@ -38,30 +39,86 @@ onMounted(() => {
         <RouterLink to="/generos"> Gênero</RouterLink>
         <RouterLink to="/categorias"> Categorias</RouterLink>
       </div>
-      <div class="botao-pesquisa"><magnify size="28" class="pesquisa-icon" /></div>
+      <div class="botao-pesquisa">
+        <magnify size="28" class="pesquisa-icon" />
+      </div>
     </nav>
   </header>
 
-  <RouterView/>  
+  <RouterView />
   <div class="LadoForm">
+
+    <div class="Carrossel">
+      <div class="Slide" v-for="(image, index) in images" :key="index" :class="{ active: index === currentIndex }">
+        <img class="ImgC" :src="image" alt="Carrossel Image">
+
+      </div>
+      <div class="titulo">
+        <p> BloomBooks</p>
+      </div>
+
+        <div class="botao-pesquisa2">
+          <button v-on:click="searchFunction" class="search-button">botao</button>
+    <input type="text" v-model="search" placeholder="Digite sua pesquisa" class="search-bar"/>
    
-        <div class="Carrossel">
-            <div class="Slide" v-for="(image, index) in images" :key="index" :class="{ active: index === currentIndex }">
-                <img class="ImgC" :src="image" alt="Carrossel Image">
-               
-            </div>
-           
-            <div class="Indicators">
-                <span v-for="(image, index) in images" :key="index" class="indicator"
-                    :class="{ active: index === currentIndex }" @click="goToSlide(index)"></span>
-            </div>
-        </div>
+  </div>
 
 
+     
+      <div class="Indicators">
+        <span v-for="(image, index) in images" :key="index" class="indicator" :class="{ active: index === currentIndex }"
+          @click="goToSlide(index)"></span>
+      </div>
     </div>
+
+
+  </div>
 </template>
 
 <style scoped>
+
+.search-bar, .search-button {
+  border-radius: 25px;
+ text-align: center;
+ justify-content: center;
+ display: flex;
+ margin-top: 150px;
+}
+
+.search-bar {
+  padding: 10px;
+  margin-right: 10px;
+  margin-left: 110px;
+  position: absolute;
+}
+
+.search-button {
+  padding: 10px 20px;
+  background-color: #BF5A5A;
+  color: white;
+  cursor: pointer;
+  position: absolute;
+ 
+ 
+ 
+  
+}
+
+.search-button:hover {
+  background-color: #bb6969;
+  position: absolute;
+}
+
+.titulo {
+  color: #BF5A5A;
+  font-family:  'Pinyon Script', cursive;
+  font-size: 200px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  position: absolute;
+}
+
 nav {
   display: flex;
   flex-direction: row;
@@ -86,15 +143,17 @@ nav {
   color: white;
   font-family: 'Poppins', sans-serif;
 }
+
 .logo-nav {
   height: 6vh;
 }
 
 a {
   text-decoration: none;
-  color:#FAF6F3;
-  
+  color: #FAF6F3;
+
 }
+
 .botao-pesquisa {
   background-color: #bf5a5a;
   border-radius: 50%;
@@ -106,62 +165,59 @@ a {
 
 
 .Carrossel {
-    position: relative;
-    width: 97vw;
-    height: 80vh;
-    overflow: hidden;
-    margin: 60px 70px 90px 24px;
-    align-items: center;
-    display: flex;
-    justify-content: center;
- 
+  position: relative;
+  width: 97vw;
+  height: 80vh;
+  overflow: hidden;
+  margin: 60px 70px 90px 24px;
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
 }
 
 .Slide {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    opacity: 0;
-    transition: opacity 0.3s ease;
-    
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  transition: opacity 0.3s ease;
+
 }
 
 .Slide.active {
-    opacity: 1;
+  opacity: 1;
 }
 
 .ImgC {
-    width: 100%;
-    height: 100%;
-    object-fit: cover;
-    border-radius: 3vh ;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  border-radius: 3vh;
 }
 
 .Indicators {
-    position: absolute;
-    align-items: center;
-    bottom: 10px;
-    left: 50%;
-    transform: translateX(-50%);
-    display: flex;
-    gap: 5px;
+  position: absolute;
+  align-items: center;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  gap: 5px;
 }
 
 .indicator {
-    width: 10px;
-    height: 10px;
-    border-radius: 50%;
-    background-color: #BF5A5A;
-    cursor: pointer;
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: #BF5A5A;
+  cursor: pointer;
 }
 
 .indicator.active {
-    background-color: #F2E8DF;
-    width: 13px;
-    height: 13px;
-}
-
-
-</style>
+  background-color: #F2E8DF;
+  width: 13px;
+  height: 13px;
+}</style>
