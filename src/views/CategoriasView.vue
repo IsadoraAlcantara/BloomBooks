c775ee755ee7df376089a7c4e08b9f808e12d2db
giulianobre <giuliarnobre@gmail.com>
2023-12-03 12:12:50 -0300

    Detalhes da pagina de favoritos

--- src/views/CategoriasView.vue
+++ src/views/CategoriasView.vue
@@ -1,9 +1,41 @@
 <script>
+import { ref } from 'vue'
+import axios from 'axios'
+
+export default {
+  setup() {
+    const favoriteBooks = ref([])
+
+    const fetchFavorites = async () => {
+      const response = await axios.get('https://gutendex.com/api/v1/favorites')
+      favoriteBooks.value = response.data
+    }
+
+    const removeFromFavorites = (id) => {
+      favoriteBooks.value = favoriteBooks.value.filter(book => book.id !== id)
+    }
+
+    fetchFavorites()
+
+    return {
+      favoriteBooks,
+      removeFromFavorites
+    }
+  }
+}
+
 </script>
 
 <template>
-    <p>Cat
-    </p>
+  <div>
+    <h1>Produtos Favoritos</h1>
+    <div v-for="book in favoriteBooks" :key="book.id">
+      <h2>{{ book.title }}</h2>
+      <p>{{ book.author }}</p>
+      <button @click="removeFromFavorites(book.id)">Remover dos favoritos</button>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
